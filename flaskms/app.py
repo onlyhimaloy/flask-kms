e3df27ea5443eb70ea22beac02befec39560f9a7
root <onlyhimaloy@gmail.com>
2017-01-06 02:19:59 +0000

    mysql query verstion

--- flaskms/app.py
+++ flaskms/app.py
@@ -5,8 +5,10 @@ from flask import Flask,render_template
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import Form
-from wtforms import StringField, SubmitField , DateField
+from wtforms import StringField, SubmitField
 from wtforms.validators import Required, Length
+from wtforms.fields.html5 import DateField
+import MySQLdb
 
 app = Flask(__name__)
 
@@ -20,35 +22,39 @@ db = SQLAlchemy(app)
 
 
 class KmsForm(Form):
-    #dt_start = DateField('Start Date', format='%Y-%m-%d' )
-    #dt_end = DateField('End Date', format='%Y-%m-%d' )
-    dt_start = StringField('Start', validators=[Required(),Length(1, 16)]) 
-    dt_end = StringField('End', validators=[Required(),Length(1, 16)])  
+    dt_start = DateField( format='%Y-%m-%d'  )
+    dt_end = DateField( format='%Y-%m-%d'  )
+
+    submit = SubmitField('Enter')
 
 
-    submit = SubmitField('Submit')
-
-
-class kms(db.Model):
-
-    __tablename__ = 'kms'
-    id = db.Column(db.Integer, primary_key=True)
-    hostname = db.Column(db.String(500),unique=False)
-    date = db.Column(db.DateTime)
-
-    def __repr__(self):
-      return '<kms {0}>'.format(self.name)
+#class Kms(db.Model):
+#
+#    __tablename__ = 'kms'
+#    id = db.Column(db.Integer, primary_key=True)
+#    hostname = db.Column(db.String(500),unique=False)
+#    date = db.Column(db.DateTime)
+#
+#    def __repr__(self):
+#      return '<kms {0}>'.format(self.name)
 
 @app.route("/", methods=['GET', 'POST'])
 
 def index():
-    dt_start = None
-    dt_end = None
     form = KmsForm()
+    kms = []   
     if form.validate_on_submit():
-      global kms	    
-      kms = kms.query.filter(kms.date.between (dt_start , dt_end ))
-    return render_template('index.html', form=form , dt_start=dt_start , dt_end=dt_end, kms=kms)
+     
+     #kms = Kms.query.filter(Kms.date.between ( form.dt_start.data ,form.dt_end.data))
+     db = MySQLdb.connect("localhost","ferozkhan","amiferoz69","server")
+     cur = db.cursor()
+     cur.execute( "SELECT * from kms where date between '%s' and '%s' " %(form.dt_start.data  , form.dt_end.data ) )
+     kms = cur.fetchall()
+
+     print (form.dt_start.data) 
+    return render_template('index.html' , form=form , kms=kms)
+
+
 
 
 if __name__ == "__main__":
