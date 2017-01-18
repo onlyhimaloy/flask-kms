bb194f7496b63c5b94b2db951346a08e76eed55e
root <onlyhimaloy@gmail.com>
2017-01-18 04:00:25 +0000

    idc-pattern

--- flaskms/app.py
+++ flaskms/app.py
@@ -1,13 +1,15 @@
 #Flask simple form application
 
 from flask import Flask,render_template
-from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 from flask_wtf import Form
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import Required, Length
 from wtforms.fields.html5 import DateField
 import MySQLdb
+
+from flask_paginate import Pagination
 
 app = Flask(__name__)
 
@@ -28,19 +30,25 @@ class KmsForm(Form):
 
 
 
-@app.route("/", methods=['GET', 'POST'])
-
-def index():
+@app.route("/", defaults={'page':1}, methods=['GET', 'POST'])
+@app.route('/page/<int:page>')
+def index(page):
+    perpage=20
+    startat=page*perpage
     form = KmsForm()
     kms = []   
-    if form.validate_on_submit():
-      db = MySQLdb.connect("localhost","ferozkhan","amiferoz69","server")
-      cur = db.cursor()
-      cur.execute( "SELECT * from kms where date between '%s' and '%s' " %(form.dt_start.data  , form.dt_end.data ) )
-      kms = cur.fetchall()
     
+    if form.validate_on_submit(): #and form.idc.choices == 1:
+        db = MySQLdb.connect("localhost","ferozkhan","amiferoz69","server")
+        cur = db.cursor()
+        #cur.execute( "SELECT * from kms where date between '%s' and '%s' " %(form.dt_start.data  , form.dt_end.data ) )
+        cur.execute ("SELECT  * from kms  where hostname regexp '[1][0-9][0-9][0-9]$' OR hostname LIKE '%%jp2v'  and date between '%s' and '%s' " %(form.dt_start.data , form.dt_end.data)  )
+
+        kms = list(cur.fetchall())
+     
     return render_template('index.html' , form=form , kms=kms)
-
-
+     
+     # Else logic
+     #Select  hostname from kms  where hostname regexp '[1][0-9][0-9][0-9]$' OR hostname LIKE '%jp2v' and date between '%s' and '%s' " %(form.dt_start.data , form.dt_end.data);
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
